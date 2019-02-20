c755bd10026aae040d8b0f1df31d8ad684618a5e
grigory.panov <gripanov@gmail.com>
2019-02-20 22:50:44 +0300

    Add lowPriorityExecutorService to process first init

--- src/main/java/jetbrains/buildServer/investigationsStatistics/NewResponsibilitiesDispatcher.java
+++ src/main/java/jetbrains/buildServer/investigationsStatistics/NewResponsibilitiesDispatcher.java
@@ -19,20 +19,44 @@ package jetbrains.buildServer.investigationsStatistics;
 import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.investigationsStatistics.common.Constants;
 import jetbrains.buildServer.responsibility.ResponsibilityEntry;
+import jetbrains.buildServer.responsibility.ResponsibilityFacadeEx;
 import jetbrains.buildServer.serverSide.BuildServerAdapter;
 import jetbrains.buildServer.serverSide.BuildServerListenerEventDispatcher;
+import jetbrains.buildServer.serverSide.ProjectManager;
 import jetbrains.buildServer.serverSide.SProject;
+import jetbrains.buildServer.serverSide.executors.ExecutorServices;
 import jetbrains.buildServer.serverSide.problems.BuildProblemInfo;
 import jetbrains.buildServer.tests.TestName;
+import jetbrains.buildServer.users.User;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.RejectedExecutionException;
 
 public class NewResponsibilitiesDispatcher {
   private static final Logger LOGGER = Logger.getInstance(NewResponsibilitiesDispatcher.class.getName());
+  @NotNull private final ExecutorServices myExecutorServices;
+  @NotNull private ResponsibilityFacadeEx myResponsibilityFacade;
+  @NotNull private final ProjectManager myProjectManager;
 
-  public NewResponsibilitiesDispatcher(@NotNull final BuildServerListenerEventDispatcher buildServerListenerEventDispatcher) {
+  public NewResponsibilitiesDispatcher(@NotNull final BuildServerListenerEventDispatcher buildServerListenerEventDispatcher,
+                                       @NotNull ExecutorServices executorServices,
+                                       @NotNull ProjectManager projectManager,
+                                       @NotNull final ResponsibilityFacadeEx responsibilityFacade) {
+    myProjectManager = projectManager;
+    myExecutorServices = executorServices;
+    myResponsibilityFacade = responsibilityFacade;
+
+    try {
+      myExecutorServices.getLowPriorityExecutorService().submit(this::processExistResponsibilities);
+    } catch (RejectedExecutionException e) {
+      // server shutdown, do nothing
+    }
+
+
     buildServerListenerEventDispatcher.addListener(new BuildServerAdapter() {
       @Override
       public void responsibleChanged(@NotNull final SProject project,
@@ -63,5 +87,12 @@ public class NewResponsibilitiesDispatcher {
         }
       }
     });
+  }
+
+  private void processExistResponsibilities() {
+    for (String projectId : myProjectManager.getProjectIds()) {
+      Map<User, List<ResponsibilityEntry>> responsibilities = myResponsibilityFacade.getResponsibilitiesMap(projectId);
+      // to smt
+    }
   }
 }
