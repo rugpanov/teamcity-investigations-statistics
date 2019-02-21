08464b3671ade1c8b922477cccd23e6b38e69e75
grigory.panov <gripanov@gmail.com>
2019-02-21 22:30:26 +0300

    remove not used code

--- src/main/java/jetbrains/buildServer/investigationsStatistics/NewResponsibilitiesDispatcher.java
+++ src/main/java/jetbrains/buildServer/investigationsStatistics/NewResponsibilitiesDispatcher.java
@@ -16,30 +16,23 @@
 
 package jetbrains.buildServer.investigationsStatistics;
 
-import com.intellij.openapi.diagnostic.Logger;
-import jetbrains.buildServer.investigationsStatistics.common.Constants;
-import jetbrains.buildServer.responsibility.ResponsibilityEntry;
 import jetbrains.buildServer.responsibility.TestNameResponsibilityEntry;
 import jetbrains.buildServer.serverSide.*;
 import jetbrains.buildServer.serverSide.executors.ExecutorServices;
-import jetbrains.buildServer.serverSide.problems.BuildProblemInfo;
-import jetbrains.buildServer.tests.TestName;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.RejectedExecutionException;
 
 public class NewResponsibilitiesDispatcher {
-  private static final Logger LOGGER = Logger.getInstance(NewResponsibilitiesDispatcher.class.getName());
   private ResponsibilitiesHolder myHolder = new ResponsibilitiesHolder();
   private final InvestigationsManager myInvestigationsManager;
-  @NotNull private final ProjectManager myProjectManager;
+  @NotNull
+  private final ProjectManager myProjectManager;
 
-  public NewResponsibilitiesDispatcher(@NotNull final BuildServerListenerEventDispatcher buildServerListenerEventDispatcher,
-                                       @NotNull final ExecutorServices executorServices,
+  public NewResponsibilitiesDispatcher(@NotNull final ExecutorServices executorServices,
                                        @NotNull final ProjectManager projectManager,
                                        @NotNull final InvestigationsManager investigationsManager) {
     myProjectManager = projectManager;
@@ -49,38 +42,6 @@ public class NewResponsibilitiesDispatcher {
     } catch (RejectedExecutionException e) {
       // server shutdown, do nothing
     }
-
-
-    buildServerListenerEventDispatcher.addListener(new BuildServerAdapter() {
-      @Override
-      public void responsibleChanged(@NotNull final SProject project,
-                                     @NotNull final Collection<TestName> testNames,
-                                     @NotNull final ResponsibilityEntry entry,
-                                     final boolean isUserAction) {
-        super.responsibleChanged(project, testNames, entry, isUserAction);
-        if (isUserAction && shouldBeReportedAsWrong(entry)) {
-          // do smt with wrong investigations
-        }
-      }
-
-      private boolean shouldBeReportedAsWrong(@Nullable final ResponsibilityEntry entry) {
-        return entry != null &&
-                entry.getReporterUser() != null &&
-                (entry.getState() == ResponsibilityEntry.State.GIVEN_UP ||
-                        entry.getState() == ResponsibilityEntry.State.TAKEN) &&
-                entry.getComment().startsWith(Constants.ASSIGNED_AUTOMATICALLY_DESCRIPTION_PREFIX);
-      }
-
-      @Override
-      public void responsibleChanged(@NotNull final SProject project,
-                                     @NotNull final Collection<BuildProblemInfo> buildProblems,
-                                     @Nullable final ResponsibilityEntry entry) {
-        super.responsibleChanged(project, buildProblems, entry);
-        if (shouldBeReportedAsWrong(entry)) {
-          // do smt with wrong investigations
-        }
-      }
-    });
   }
 
   private void processExistResponsibilities() {
